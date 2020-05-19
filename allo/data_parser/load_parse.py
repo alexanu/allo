b5af831b6424d3163cd230136b74018ab7ee22fc
junyitt <junyitt@cxtanalytics.com>
2020-05-19 18:35:22 +0800

    added parse barclay hfi

--- allo/data_parser/load_parse.py
+++ allo/data_parser/load_parse.py
@@ -25,7 +25,29 @@ def trpei():
     tdf = pd.Series(target_ret_dict)
     tdf = pd.DataFrame(tdf)
     tdf.columns = ["TRPEI"]
+    return target_ret_dict, tdf
 
+
+def barclay_hfi():
+    """Load TRPEI target returns data
+    """
+    df = pd.read_csv("data_parser/Barclay_HFI.csv")
+    df.columns = ["Year"] + [j+1 for j in range(12)] 
+    # df = df.drop(["Temp"], axis = 1)
+    df.set_index("Year", inplace = True)
+
+    first_year = int(df.index[0])
+    target_ret_dict = {}
+    for i in range(df.shape[0]):
+        for j in range(df.shape[1]):
+            dt = datetime.datetime(first_year + i, 1+j, 1) + relativedelta(months = 1) - datetime.timedelta(days = 1) 
+            raw_val = df.iloc[i,j]
+            if not df.isna().iloc[i,j]:
+                ret = float(raw_val.replace("%", ""))/100
+                target_ret_dict[dt] = ret
+    tdf = pd.Series(target_ret_dict)
+    tdf = pd.DataFrame(tdf)
+    tdf.columns = ["Barclay_HFI"]
     return target_ret_dict, tdf
 
 
---
+++ allo/yaml_config/todo.md
@@ -0,0 +1,21 @@
+TRPEI 
+Basket
+BAM
+BX
+KKR
+APO
+KW
+HTGC
+CG
+OAK
+IWN
+
+Barclay Hedge Fund Index (HFI) 
+basket = ['VTV','VOE','MGV','IWD','IVE','IWS','IJS','IWN','IJJ','SCHV','IVLU','EWJV','IEVL',\
+          'CV9','VAL','EWJV','MTUM','PDP','ONEO','PIZ','VFMO','IMTM','GMOM','FDMO','QMOM','IMOM',\
+          'QMOM','SPMO','QMOM','AGG','BND','LQD','VSCH','BSV','TIP','BNDX','EMB','TLT','ZROZ','VCIT',\
+          'IEF','SHY','TMF','TYD','EMB','EMLC','KCCB','CBON','VNQ','VNQI','REZ','FLCH','CHIR','PDBC',\
+          'COPX','DBC','USO','GSG','DBA','GLD','IAU','SLV','GDXJ','GDX','RING','HDGE','SH','PSQ','RWM',\
+          'SBB','DOG','MYY','EUM','EFZ','CHAD','YXI','TAIL','TVIX','VXX','UVXY','GLD','YGRN','GNAF','EMTY',\
+          'SMN','SZK','SCC','SKF','RXD','SIJ','SRS','SSG','DUG','REW','SDP','SJB','TBF','SJB','DTYS','TBX',\
+          'TAPR','TBT','TMV','PST','TYO','DTUS','DFVS','EFA','EEM']
